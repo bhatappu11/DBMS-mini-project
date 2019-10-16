31ad8c94003ad8fb24682f8de3589431017de2f0
bhatappu11 <arpithabht12@gmail.com>
2019-10-17 04:45:40 +0530

    updating contractor_details

--- contractor_details.php
+++ contractor_details.php
@@ -48,75 +48,36 @@
     <div>
 		<h3> <br><br> </h3>
     </div>
-
-    <?php
-/*		$query = "select * from builder";
-				//echo $query;
-                $query_run = mysqli_query($con,$query);
-  */              $query1 = usp_ConvertQuery2HTMLTable('SELECT Builder_id, Builder_name FROM builder_database.builder');
-                $query_run = mysqli_query($con,$query1);
-                /*while($row = mysqli_fetch_array($query_run)) {
-                    echo $row['Builder_id'];
-                    echo $row['Builder_name'];
-                }*/
-
-                CREATE PROC builder_database.usp_ConvertQuery2HTMLTable (@SQLQuery NVARCHAR(3000))
-                AS
-                BEGIN
-                DECLARE @columnslist NVARCHAR (1000) = ''
-                DECLARE @restOfQuery NVARCHAR (2000) = ''
-                DECLARE @DynTSQL NVARCHAR (3000)
-                DECLARE @FROMPOS INT
-
-                SET NOCOUNT ON
-
-                SELECT @columnslist += 'ISNULL (' + NAME + ',' + '''' + ' ' + '''' + ')' + ','
-                FROM sys.dm_exec_describe_first_result_set(@SQLQuery, NULL, 0)
-
-                SET @columnslist = left (@columnslist, Len (@columnslist) - 1)
-                SET @FROMPOS = CHARINDEX ('FROM', @SQLQuery, 1)
-                SET @restOfQuery = SUBSTRING(@SQLQuery, @FROMPOS, LEN(@SQLQuery) - @FROMPOS + 1)
-                SET @columnslist = Replace (@columnslist, '),', ') as TD,')
-                SET @columnslist += ' as TD'
-                SET @DynTSQL = CONCAT (
-                        'SELECT (SELECT '
-                        , @columnslist
-                        ,' '
-                        , @restOfQuery
-                        ,' FOR XML RAW (''TR''), ELEMENTS, TYPE) AS ''TBODY'''
-                        ,' FOR XML PATH (''''), ROOT (''TABLE'')'
-                        )
-
-                EXEC (@DynTSQL)
-                SET NOCOUNT OFF
-                END
-                GO;
-				//echo mysql_num_rows($query_run);
-				//if($query_run)
-				//{
-	/*				if(mysqli_num_rows($query_run)>0)
-					{
-					$row = mysqli_fetch_array($query_run,MYSQLI_ASSOC);
-					
-					$_SESSION['username'] = $username;
-					$_SESSION['password'] = $password;
-					
-					header( "Location: homepage.php");
-					}
-					else
-					{
-						echo '<script type="text/javascript">alert("No such User exists. Invalid Credentials")</script>';
-					}
-				}
-				else
-				{
-					echo '<script type="text/javascript">alert("Database Error")</script>';
-				}
-			}
-			else
-			{
-            		}
-		*/?>
+    <div class="container" style="margin-top:30px">
+    <center>
+        <table class="table table-striped table-dark">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Username</th>
+                <th>Password</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                
+                $sql = "SELECT * FROM builder";
+                $query_run = mysqli_query($con,$sql);
+        
+            
+                if(mysqli_num_rows($query_run)>0) {
+                    // output data of each row
+                    while($row = $query_run->fetch_assoc()) {
+                        echo "<tr><td>" . $row["Builder_id"]. "</td><td>" . $row["Builder_name"] . "</td><td>"
+                        . $row["password"]. "</td></tr>";
+                    }
+                    echo "</table>";
+                } else { echo "0 results"; } 
+            ?>
+        </tbody>
+        </table>
+    </center>
+    </div>
     
 </body>
 </html>
