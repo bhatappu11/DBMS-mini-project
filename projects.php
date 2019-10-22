9dae029d27776efa742539910e6aa2a08214baed
bhatappu11 <arpithabht12@gmail.com>
2019-10-22 22:52:13 +0530

    updating the project file

--- projects.php
+++ projects.php
@@ -39,103 +39,71 @@
             </ul>
         </div>
 	</nav>
+    
     <div>
 		<h3> <br><br> </h3>
     </div>
-    <div class="form-inline">
-        <div class="form-group">
-            <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" id="strikes-range" data-toggle="dropdown" aria-haspopup="true"> Add Projects <span class="caret"></span> </button>
-                <ul class="dropdown-menu" aria-labelledby="strikes">
-                    <li style="width: 280px;">
-                        <form class="form-horizontal" action="contractor_details.php" style="display:block;">
-                            <div class="form-group donotchange">
-                                <label for="strike-from" class="col-sm-2 control-label">Project ID</label>
-                                <div class="col-xs-8">
-                                    <input type="text" name="project_id" placeholder="Enter project ID">
-                                </div>
-                            </div>
-                            <div class="form-group donotchange">
-                                <label for="strike-to" class="col-sm-2 control-label">Project Name</label>
-                                <div class="col-xs-8">
-                                    <input type="text" name="project_name" placeholder="Enter project name">
-                                </div>
-                            </div>
-                            <div class="form-group donotchange">
-                                <label for="strike-to" class="col-sm-2 control-label">Contractor ID</label>
-                                <div class="col-xs-8">
-                                    <input type="text" name="contractor_id" placeholder="Enter contractor id">
-                                </div>
-                            </div>
-                            <div class="form-group donotchange">
-                                <div class="col-sm-offset-2 col-sm-10">
-                                    <button name="add_project" type="submit" class="btn btn-submit">Add</button>
-                                </div>
-                            </div>
-                        </form>
-                        <?php
-                            if(isset($_POST['add_project']))
-                            {
-                                @$project_id=$POST['project_id'];
-                                @$project_name=$_POST['project_name'];
-                                
-                                $username=$_SESSION['username'];
-                                $query = "select Builder_id from builder where Builder_name='$username'";
-                                $query_run=mysqli_query($con,$query);
-                                $ans = mysqli_fetch_assoc($query_run);
-                                $id = $ans['Builder_id'];
+    <div>
+	<center><h2>Add Projects</h2></center>
+		<form action="projects.php" method="post">
+			<div>
+				<label><b>Project ID</b></label>
+				<input type="text" placeholder="Enter project id" name="project_id" required>
+				<label><b>Project Name</b></label>
+				<input type="text" placeholder="Enter project name" name="project_name" required>
+                <label><b>Contractor ID</b></label>
+				<input type="text" placeholder="Enter contractor id" name="cont_id" required>
+				<button name="add_project" class="sign_up_btn" type="submit">Add</button>
+				
+			</div>
+		</form>
+		
+		<?php
+			if(isset($_POST['add_project']))
+			{
+                $username=$_SESSION['username'];
+                $query = "select Builder_id from builder where Builder_name='$username'";
+                $query_run=mysqli_query($con,$query);
+                $ans = mysqli_fetch_assoc($query_run);
+                $id = $ans['Builder_id'];
+                
 
-                                $query2 = "select Contractor_id from contractor where Builder_id='$id'";
-                                $query_run2=mysqli_query($con,$query2);
-                                $ans2 = mysqli_fetch_assoc($query_run2);
-                                $id2 = $ans['Contractor_id'];
-
-                                $query1 = "select * from projects where Builder_id='$id'";
-                                //echo $query;
-                                $query_run1 = mysqli_query($con,$query1);
-                                //echo mysql_num_rows($query_run);
-                                if($query_run1)
-                                {
-                                    if(mysqli_num_rows($query_run1)>0)
-                                    {
-                                       
-                                        echo '<script type="text/javascript">alert("Contractor already exists!")</script>';
-                                    }
-                                    else
-                                    {   
-                                    
-                                        $query2 = "insert into projects values('$project_id','$project_name','$id','$id2')";
-                                        $query_run2 = mysqli_query($con,$query2);
-                                        if($query_run2)
-                                        {
-                                            echo '<script type="text/javascript">alert("Contractor added..")</script>';
-                                            $_SESSION['username'] = $username;
-                                            $_SESSION['password'] = $password;
-                                            $_SESSION['cont-name'] = $cont_name;
-                                            
-                                        }
-                                        else
-                                        {
-                                            echo '<p class="bg-danger msg-block">Unsuccessful due to server error. Please try later</p>';
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    echo '<script type="text/javascript">alert("DB error")</script>';
-                                }
-                            }
-                            else
-                            {
-                                
-                            }
-                        ?>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div id="push"></div>
+				//$query = "select * from contractor where Builder_id='$id' and Contractor_name='$contractor_name'";
+				//echo $query;
+				//$query_run = mysqli_query($con,$query);
+				//echo mysql_num_rows($query_run);
+				//if($query_run)
+					// {
+					// 	if(mysqli_num_rows($query_run)>0)
+					// 	{
+					// 		echo '<script type="text/javascript">alert("This Username Already exists.. Please try another username!")</script>';
+					// 	}
+					// 	else
+					// 	{
+							$query = "insert into projects (project_id,project_name,Builder_id,Contractor_id) values('". $_POST["project_id"] . "','". $_POST["project_name"] . "','$id','". $_POST["cont_id"] . "')";
+							$query_run = mysqli_query($con,$query);
+							if($query_run)
+							{
+								echo '<script type="text/javascript">alert("Contractor Registered.. Welcome")</script>';
+								header( "Location: projects.php");
+							}
+							else
+							{
+								echo '<p class="bg-danger msg-block">Registration Unsuccessful due to server error. Please try later</p>';
+							}
+						}
+			// 		}
+			// 		else
+			// 		{
+			// 			echo '<script type="text/javascript">alert("DB error")</script>';
+			// 		}
+				
+			// }
+			else
+			{
+			}
+		?>
+	</div>
     <div class="container" style="margin-top:30px">
         <table class="table table-striped table-dark table-bordered text-center">
         <thead>
@@ -167,7 +135,7 @@
                         . $row["Contractor_id"]. "</td></tr>";
                     }
                     echo "</table>";
-                } else { echo "empty results"; }
+                } else { echo "0 results"; }
             ?>
         </tbody>
         </table>
@@ -178,6 +146,5 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    </body>
-    </html>
+</body>
+</html>
