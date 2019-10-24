c73d1ff32d0d8571f59a12ed71833e0e01a325e5
bhatappu11 <arpithabht12@gmail.com>
2019-10-24 15:27:07 +0530

    updating

--- personal_schedule.php
+++ personal_schedule.php
@@ -49,15 +49,15 @@
             <button class="btn btn-default dropdown-toggle" type="button" id="strikes-range" data-toggle="dropdown" aria-haspopup="true"> Add to schedule <span class="caret"></span> </button>
                 <ul class="dropdown-menu" aria-labelledby="strikes">
                     <li style="width: 280px;">
-            		    <form action="projects.php" method="post">
+            		    <form action="personal_schedule.php" method="post">
 			                <div class="class-for-form">
                                 <label><b>Type_of_work</b></label>
                                 <input type="text" class="input-class" placeholder="Enter type_of_work" name="work_name" required>
                                 <label><b>Date</b></label>
-                                <input type="text" class="input-class" placeholder="Enter Date in format YYYY-MM-DD" name="date" required>
-                                <label><b>Time</b></label>
-                                <input type="text" class="input-class" placeholder="Enter Time " name="time" required>
-                                <button name="add_schedule" class="btn-submit" type="submit">Add</button>
+                                <input type="text" class="input-class" placeholder="Enter Date in format YYYY-MM-DD" name="date_due" required>
+                                <!-- <label><b>Time</b></label>
+                                <input type="text" class="input-class" placeholder="Enter Time " name="time" required> -->
+                                <button name="add_to_schedule" class="btn-submit" type="submit">Add</button>
 				            </div>
 		                </form>
                     </li>
@@ -66,49 +66,49 @@
         </div>
 		
 		<?php
-			if(isset($_POST['add_schedule']))
+			if(isset($_POST['add_to_schedule']))
 			{
                 $type_of_work=$_POST['work_name'];
-                $date=$_POST['date'];
-                $time=$_POST['time'];
+                $date_due=$_POST['date_due'];
+                //$time=$_POST['time'];
                 $username=$_SESSION['username'];
                 $query = "select Builder_id from builder where Builder_name='$username'";
                 $query_run=mysqli_query($con,$query);
                 $ans = mysqli_fetch_assoc($query_run);
                 $id = $ans['Builder_id'];
 
-                $cont_name = $_POST['cont_name'];
-				$query = "select * from contractor where Builder_id='$id' and Contractor_name='$cont_name'";
-				//echo $query;
-				$query_run = mysqli_query($con,$query);
-				//echo mysql_num_rows($query_run);
-				if($query_run)
-                {
-                    if(mysqli_num_rows($query_run)>0)
-                    {
-                        echo '<script type="text/javascript">alert("This Username Already exists.. Please try another username!")</script>';
-                    }
-                    else
-                    {
-                        $query = "insert into personal (Builder_id,Type_of_work,Date,Time) values('$id','". $_POST["work_name"] . "'," . $_POST["date"] . "'," . $_POST["time"] . "')";
+                // $cont_name = $_POST['cont_name'];
+				// $query = "select * from contractor where Builder_id='$id' and Contractor_name='$cont_name'";
+				// echo $query;
+				// $query_run = mysqli_query($con,$query);
+				// echo mysql_num_rows($query_run);
+				// if($query_run)
+                // {
+                //     if(mysqli_num_rows($query_run)>0)
+                //     {
+                //         echo '<script type="text/javascript">alert("This Username Already exists.. Please try another username!")</script>';
+                //     }
+                //     else
+                //     {
+                        $query = "insert into personal_schedule values('$id',' " . $_POST['work_name'] . " ',' " . $_POST['date_due'] . " ')";//," . $_POST["time"] . "')";
                         $query_run = mysqli_query($con,$query);
                         if($query_run)
                         {
-                            echo '<script type="text/javascript">alert("Contractor Registered.. Welcome")</script>';
-                            header( "Location: contractor_details.php");
+                            echo '<script type="text/javascript">alert("Schedule Added... ")</script>';
+                            header( "Location: personal_schedule.php");
                         }
                         else
                         {
                             echo '<p class="bg-danger msg-block">Registration Unsuccessful due to server error. Please try later</p>';
                         }
-                    }
-                }
-                else
-                {
-                    echo '<script type="text/javascript">alert("DB error")</script>';
-                }
+                    // }
+            //     }
+            //     else
+            //     {
+            //         echo '<script type="text/javascript">alert("DB error")</script>';
+            //     }
 				
-			}
+			 }
 			else
 			{
 			}
@@ -121,7 +121,7 @@
                 <th>Builder ID</th>
                 <th>Type of work</th>
                 <th>Date</th>
-                <th>Time</th>
+                <!-- <th>Time</th> -->
             </tr>
         </thead>
         <tbody>
@@ -142,8 +142,8 @@
                 if(mysqli_num_rows($query_run)>0) {
                     // output data of each rows
                     while($row = $query_run->fetch_assoc()) {
-                        echo "<tr><td>" . $row["Builder_id"]. "</td><td>" . $row["work_name"] . "</td><td>"
-                        . $row["date"]. "</td></tr>" . $row["time"]. "</td></tr>";
+                        echo "<tr><td>" . $row["Builder_id"]. "</td><td>" . $row["Type_of_work"] . "</td><td>"
+                        . $row["Date"]. "</td></tr>"; //. $row["time"]. "</td></tr>";
                     }
                     echo "</table>";
                 } else { echo "0 results"; }
